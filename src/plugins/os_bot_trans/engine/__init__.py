e726bdde349095fd7ad7b44c516f775877f127b9
chenxuan <chenxuan330@163.com>
2022-12-20 17:09:20 +0800

    优化翻译时的网址过滤

--- src/plugins/os_bot_trans/engine/__init__.py
+++ src/plugins/os_bot_trans/engine/__init__.py
@@ -118,14 +118,14 @@ class Engine(abc.ABC):
     def change_dict(self):
         return self._change_dict
 
-http_match = re.compile(r'[http|https]*://[a-zA-Z0-9.?/&=:]*', re.S)
+http_match = re.compile(r'(((http|ftp|https):\/{2})?(([0-9a-z_-]+\.)+(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)(:[0-9]{1,5})?(\/(([~0-9a-zA-Z\#\+\%@\.\/_-]+))?(\?[0-9a-zA-Z\+\%@\/&\[\];=_-]+)?)?))', re.I)
 
 
 def deal_trans_text(text):
     """
         移除翻译字符串中可能影响翻译结果的内容。
     """
-    text = http_match.sub('', text)  # 移除网址
+    text = http_match.sub(' ', text)  # 移除网址
     text = text.strip()  # 移除空白字符串
     text = EmojiFilter.filter(text)  # 移除emoji
     return text
